d9d720bf3459c5e0f667603474fc62ae7700b0df
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-07-14 22:30:14 -0700

    added user class and worked in to posts updated nav bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,11 +16,50 @@ class Post(db.Model):
     title = db.Column(db.String(100))
     body = db.Column(db.String(1000))
     likes = db.Column(db.Integer)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.likes = 0
+        self.owner = owner
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(40))
+    posts = db.relationship('Post', backref='owner')
+
+    def __init__(self, username, email, password):
+        self.email = email
+        self.password = password
+        self.username = username
+
+def validate(un, em, p, p2):
+    error = {0:"", 1:"", 2:"", 3:"", 5:em, 6:un}
+    if len(un) < 6:
+        error[0] = "username must be 6 or more characters"
+
+    if len(p) < 6:
+        error[1] = "password must be 6 or more characters"
+    elif " " in p:
+        error[1] = "no spaces please"
+    
+    if p2 == "":
+        error[2] = "please re-enter your password"
+    elif p2 != p:
+        error[2] = "password must match"
+
+    emat = re.search("\w+@\w+.\w+", em)
+    if not emat:
+        error[3] = "please enter a valid email"
+        error[5] = ""
+
+    return error
+
 
 @app.route('/login/signup')
 def signup():
@@ -29,7 +68,17 @@ def signup():
 
 @app.route("/login")
 def login():
-
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash("logged in", 'success')
+            return redirect('/blog')
+        else:
+            flash('email or password inccorect', 'error')
+            
     return render_template('login.html')
 
 @app.route('/')
