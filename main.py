9f9d9553fa1bfab34741433fcd570121b8a65bfe
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-07-15 19:17:48 -0700

    added login, signup, and logout functionality, also added ability to view posts by user

--- main.py
+++ main.py
@@ -42,6 +42,9 @@ def validate(un, em, p, p2):
     error = {0:"", 1:"", 2:"", 3:"", 5:em, 6:un}
     if len(un) < 6:
         error[0] = "username must be 6 or more characters"
+    elif " " in un:
+        error[0] = "no spaces please"
+        error[6] = ""
 
     if len(p) < 6:
         error[1] = "password must be 6 or more characters"
@@ -52,32 +55,71 @@ def validate(un, em, p, p2):
         error[2] = "please re-enter your password"
     elif p2 != p:
         error[2] = "password must match"
-
-    emat = re.search("\w+@\w+.\w+", em)
-    if not emat:
-        error[3] = "please enter a valid email"
-        error[5] = ""
+    if em != "":
+        emat = re.search("\w+@\w+.\w+", em)
+        if not emat:
+            error[3] = "please enter a valid email"
+            error[5] = ""
 
     return error
 
 
-@app.route('/login/signup')
+@app.route('/login/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        error = validate(username, email, password, verify)
+        if error[0] or error[1] or error[2] or error[3]:
+            if error[0] != "":
+                flash(error[0], 'error')
+            if error[3] != "":
+                flash(error[3], 'error')
+            if error[1] != "":
+                flash(error[1], 'error')
+            if error[2] != "": 
+                flash(error[2], 'error')
+            return render_template("signup.html", username=error[6], email=error[5])
+            
+        
+        else:
+            user = User.query.filter_by(username=username).first()
+            if not user:
+                user = User.query.filter_by(email=email).first()
+                if not user:
+                    new_user = User(username, email, password)
+                    db.session.add(new_user)
+                    db.session.commit()
+                    session['user'] = username
+                    return redirect('/')
+                else:
+                    flash('Cannot create (Email already in use)', 'error')
+            else:
+                flash('Cannot create (Username already in use)', 'error')
 
     return render_template("signup.html")
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        ename = request.form['ename']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=ename).first()
         if user and user.password == password:
-            session['email'] = email
+            session['user'] = ename
             flash("logged in", 'success')
             return redirect('/blog')
         else:
-            flash('email or password inccorect', 'error')
+            user = User.query.filter_by(username=ename).first()
+            if user and user.password == password:
+                session['user'] = ename
+                flash("logged in", 'success')
+                return redirect('/blog')
+            else:
+                flash('Email/Username or Password inccorect', 'error')
             
     return render_template('login.html')
 
@@ -91,27 +133,32 @@ def blog():
     postid = request.args.get('id')
     userid = request.args.get('uid')
     if postid:
-        post = Post.query.get(num)
+        post = Post.query.get(postid)
         return render_template('postit.html', post=post)
     if userid:
-        #userposts = Post.query.filter_by(userid=userid)
-        return render_template('author.html')#, posts=userposts)
-    else:
-        posts = Post.query.all()
-        return render_template('blog.html', title="Blog", posts=posts)  
+        userposts = Post.query.filter_by(owner_id=userid).all()
+        return render_template('author.html', posts=userposts)
+
+    posts = Post.query.all()
+    return render_template('blog.html', title="Blog", posts=posts)  
  
 @app.route('/post_form', methods=["POST", "GET"])   
 def post_form():
+    if 'user' not in session:
+        return redirect('/login')
     if request.method == "POST":
+        owner = User.query.filter_by(username=session['user']).first()
+        if not owner:
+            owner = User.query.filter_by(email=session['user']).first()
         title = request.form["title"]
         body = request.form["body"]
         if body == "" or title == "":
             flash("please have content in both fields", "error")
             return redirect("/post_form")
-        new_post = Post(title, body)
+        new_post = Post(title, body, owner)
         db.session.add(new_post)
         db.session.commit()
-        return redirect("/post?id=" + str(new_post.id))
+        return redirect("/blog?id=" + str(new_post.id))
 
     return render_template("post_form.html", title="New Post")
 
@@ -125,6 +172,11 @@ def delete():
         db.session.commit()
     return redirect('/blog')
 
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        del session['user']
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
